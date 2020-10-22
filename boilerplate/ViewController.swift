3ab59664fa3aca4c5d4ec2618beefb4d1481ae5d
Marvin <Marvinegoavilz@gmail.com>
2020-10-22 02:54:47 +0100

    Initial Commit

---
+++ boilerplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  boilerplate
+//
+//  Created by user177281 on 22/10/2020.
+//  Copyright © 2020 Marvin Malvados y Asociados. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
